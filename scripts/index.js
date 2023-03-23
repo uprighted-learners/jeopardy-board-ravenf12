2d5ce3911f4b0a5625cbabf1054b5154acfdcfc8
Raven Fisher <ravenfisher1122@gmail.com>
2023-03-22 22:03:03 -0600

    Trying to Finish

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,18 @@
 // Do not change the import statement
+// This statment imports the exported file so it's contents are accessible to us.
+// This makes the "placeholderQuestions" act like a variable essentially.
 import placeholderQuestions from "./placeholder-questions.js";
+// Is an object who's content are the file data
 console.log({ placeholderQuestions });
+console.log(placeholderQuestions[0]);
+// When I need a question/Answer I can iterate over the array.
+
+
+  document.addEventListener("submit", e => {
+  })
+
+  const displaySalt = (placeholderQuestions) => {
+    const salt = document.placeholderQuestions[1];
+    console.log(Salt);
+  }
+
